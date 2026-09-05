d3c4be50d12b07dd395b2798769a6399d09f5d31
Jason Christopherson <jchristopherson@hotmail.com>
2026-09-05 05:53:45 -0500

    Update example

--- examples/four_bar_example_1.f90
+++ examples/four_bar_example_1.f90
@@ -6,11 +6,13 @@
 program example
     use iso_fortran_env
     use dynamics
+    use fplot_core
+    use linalg, only : identity
     implicit none
 
     ! Parameters
     real(real64), parameter :: pi = 2.0d0 * acos(0.0d0)
-    integer(int32), parameter :: npts = 21
+    integer(int32), parameter :: npts = 100
 
     ! Model Properties
     real(real64), parameter :: crank = 1.0d0
@@ -23,7 +25,8 @@ program example
     type(link_container) :: links(4)
     type(joint) :: joints(4)
     type(planar_linkage) :: linkage
-    real(real64) :: theta, p(3)
+    real(real64) :: theta(npts), p(npts,3)
+    real(real64), allocatable, dimension(:) :: plt_config
     real(real64), allocatable, dimension(:,:) :: jac
 
     ! Define the links.  Each link carries a joint frame at each end, with the
@@ -55,20 +58,23 @@ program example
     call linkage%set_configuration([0.0d0, 0.5d0 * pi, -0.5d0 * pi, 0.0d0])
 
     ! Sweep the crank and trace the coupler point
-    print "(A)", new_line('a') // "  CRANK          X          Y      ANGLE"
+    theta = linspace(0.0d0, 2.0d0 * pi, npts)
     do i = 1, npts
-        theta = 2.0d0 * pi * real(i - 1, real64) / real(npts - 1, real64)
-        p = linkage%end_effector_pose([theta])
-        print "(F7.3, 3F11.4)", theta, p(1), p(2), p(3)
+        p(i,:) = linkage%end_effector_pose([theta(i)])
     end do
 
     ! Examine the sensitivity of the coupler point to the crank at mid-stroke
-    theta = 0.5d0 * pi
-    jac = linkage%jacobian([theta])
+    jac = linkage%jacobian([0.5d0 * pi])
     print "(A)", new_line('a') // "Jacobian at a crank angle of 90 degrees:"
     do i = 1, size(jac, 1)
         print *, jac(i,:)
     end do
+
+    ! Plot the coupler curve along with the linkage. The complete set of joint 
+    ! variables, not just the actuated variable, is required in order to locate 
+    ! each link.
+    plt_config = linkage%solve_configuration([0.25d0 * pi])
+    call plot_coupler_path(p, linkage, plt_config)
 
 contains
     ! Constructs a planar link of the requested length carrying a joint frame at
@@ -86,11 +92,90 @@ contains
     function translation(a) result(rst)
         real(real64), intent(in) :: a
         real(real64) :: rst(4, 4)
-        integer(int32) :: i
-        rst = 0.0d0
-        do i = 1, 4
-            rst(i,i) = 1.0d0
-        end do
+        rst = identity(4)
         rst(1,4) = a
     end function
+
+    ! Coupler Path Plot
+    subroutine plot_coupler_path(pts, mech, q)
+        real(real64), intent(in), dimension(:,:) :: pts
+        class(kinematic_mechanism), intent(in), target :: mech
+        real(real64), intent(in), dimension(:) :: q
+
+        type(plot_2d) :: plt
+        type(legend), pointer :: lgnd
+
+        call plt%initialize()
+        call plt%set_x_axis_title("x")
+        call plt%set_y_axis_title("y")
+        lgnd => plt%get_legend()
+        call lgnd%set_is_visible(.true.)
+        call lgnd%set_horizontal_position(LEGEND_LEFT)
+        call lgnd%set_vertical_position(LEGEND_BOTTOM)
+        call lgnd%set_draw_border(.false.)
+        call lgnd%set_layout(LEGEND_ARRANGE_HORIZONTALLY)
+        call lgnd%set_draw_inside_axes(.false.)
+
+        ! Coupler Curve
+        call plt%push(pts(:,1), pts(:,2), name = "Coupler Curve")
+
+        ! The linkage in its requested configuration
+        call draw_linkage(plt, mech, q)
+
+        call plt%draw()
+    end subroutine
+
+    ! Draws each link as a polyline passing through the joint frames the link
+    ! carries.  The location of each frame is found by transforming the frame,
+    ! which is expressed in the link's body coordinate frame, into the frame of
+    ! the base link.
+    subroutine draw_linkage(plt, mech, q)
+        type(plot_2d), intent(inout) :: plt
+        class(kinematic_mechanism), intent(in), target :: mech
+        real(real64), intent(in), dimension(:) :: q
+
+        integer(int32) :: j, k, n
+        class(link), pointer :: lnk
+        type(plot_data_2d), allocatable, dimension(:) :: pd
+        real(real64) :: T(4, 4), Pk(4, 4)
+        real(real64), allocatable, dimension(:) :: x, y
+        character(len = :), allocatable :: name
+
+        allocate(pd(mech%get_link_count()))
+        do j = 1, mech%get_link_count()
+            ! Get the link and compute its location and orientation
+            lnk => mech%get_link(j)
+            n = lnk%get_joint_count()
+            T = mech%body_transform(j, q)
+            allocate(x(n), y(n))
+            do k = 1, n
+                Pk = matmul(T, lnk%get_joint_frame(k))
+                x(k) = Pk(1,4)
+                y(k) = Pk(2,4)
+            end do
+
+            ! Define a name for the link
+            select case (j)
+            case (1)
+                name = "Ground Link"
+            case (2)
+                name = "Crank"
+            case (3)
+                name = "Coupler"
+            case (4)
+                name = "Rocker"
+            end select
+
+            ! Plot the link
+            call pd(j)%define_data(x, y)
+            call pd(j)%set_line_width(3.0)
+            if (j == 1) call pd(j)%set_line_style(LINE_DASHED)
+            call pd(j)%set_draw_markers(.true.)
+            call pd(j)%set_marker_style(MARKER_EMPTY_CIRCLE)
+            call pd(j)%set_name(name)
+            call plt%push(pd(j))
+
+            deallocate(x, y)
+        end do
+    end subroutine
 end program
