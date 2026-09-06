0f83e7f326b6e12894498c3ae7b89fe9b2d5ec8e
jchristopherson <jchristopherson@hotmail.com>
2026-09-06 02:15:15 +0000

    Deploying to gh-pages from  @ ef2f178563dd17230c7fa29db8f14ee51d14e707 🚀

--- c-api/topics.js
+++ c-api/topics.js
@@ -1,11 +1,11 @@
 var topics =
 [
-    [ "Public constants", "group__dynamics__constants.html", "group__dynamics__constants" ],
-    [ "Matrix and general kinematics", "group__dynamics__matrix.html", "group__dynamics__matrix" ],
-    [ "Frequency response and system identification", "group__dynamics__frequency.html", "group__dynamics__frequency" ],
-    [ "Quaternion operations", "group__dynamics__quaternion.html", "group__dynamics__quaternion" ],
-    [ "Geometry operations", "group__dynamics__geometry.html", "group__dynamics__geometry" ],
-    [ "Serial linkage operations", "group__dynamics__serial.html", "group__dynamics__serial" ],
-    [ "Parallel and planar linkage operations", "group__dynamics__parallel.html", "group__dynamics__parallel" ],
-    [ "Transfer functions and state-space models", "group__dynamics__state.html", "group__dynamics__state" ]
+    [ "Public constants", "group__dynamics__constants.html", null ],
+    [ "Matrix and general kinematics", "group__dynamics__matrix.html", null ],
+    [ "Frequency response and system identification", "group__dynamics__frequency.html", null ],
+    [ "Quaternion operations", "group__dynamics__quaternion.html", null ],
+    [ "Geometry operations", "group__dynamics__geometry.html", null ],
+    [ "Serial linkage operations", "group__dynamics__serial.html", null ],
+    [ "Parallel and planar linkage operations", "group__dynamics__parallel.html", null ],
+    [ "Transfer functions and state-space models", "group__dynamics__state.html", null ]
 ];
