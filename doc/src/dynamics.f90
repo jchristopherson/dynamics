5e96ff3fda793d98a2441d1652ed8aca0421d7ce
Jason Christopherson <jchristopherson@hotmail.com>
2026-09-18 14:07:58 -0500

    Update documentation

--- doc/src/dynamics.f90
+++ doc/src/dynamics.f90
@@ -14,5 +14,8 @@ module dynamics
     use dynamics_parallel_linkage
     use dynamics_geometry
     use dynamics_quaternions
+    use dynamics_rigid_bodies
     use dynamics_line_elements
+    use dynamics_variational_integrators
+    use dynamics_linkage_dynamics
 end module
