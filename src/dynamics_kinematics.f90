803faa7d53a5e35f45ed2d17e671a17f9e34911b
jchristopherson <jchristopherson@hotmail.com>
2026-09-23 10:47:17 +0000

    Deploying to gh-pages from  @ c92fd0a3cff9ee316a7f26a9e29a31e2a6a60c71 🚀

--- src/dynamics_kinematics.f90
+++ src/dynamics_kinematics.f90
@@ -1,3 +1,16 @@
+! Copyright (c) 2022-2026 Jason Christopherson
+! SPDX-License-Identifier: MIT
+!
+! Permission is hereby granted, free of charge, to any person obtaining a copy
+! of this software and associated documentation files (the "Software"), to deal
+! in the Software without restriction, including without limitation the rights
+! to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+! copies of the Software, and to permit persons to whom the Software is
+! furnished to do so, subject to the following conditions:
+!
+! The Software is provided "as is", without warranty of any kind, express or
+! implied, including but not limited to the warranties of merchantability,
+! fitness for a particular purpose and noninfringement.
 module dynamics_kinematics
     use iso_fortran_env
     use nonlin
